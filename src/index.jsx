36a70ce777ee7542963208cbf9e89fd39194d43d
Camille Crapat <camille.capt@gmail.com>
2023-04-01 18:28:43 +0200

    ajout des messages toasts

--- src/index.jsx
+++ src/index.jsx
@@ -11,18 +11,25 @@ import FirstConnexion from "./pages/FirstConnexion";
 import AllPosts from "./pages/AllPosts";
 import Error from "./pages/Error";
 
+import Toasts from "./components/Toast";
+
+import { ToastsProvider } from "./utils/context";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/firstconnexion" element={<FirstConnexion />} />
-        <Route path="/allposts" element={<AllPosts />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <ToastsProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/firstconnexion" element={<FirstConnexion />} />
+          <Route path="/allposts" element={<AllPosts />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Toasts />
+        <Footer />
+      </Router>
+    </ToastsProvider>
   </React.StrictMode>
 );

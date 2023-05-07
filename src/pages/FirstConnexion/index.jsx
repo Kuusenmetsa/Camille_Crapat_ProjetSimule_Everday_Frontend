eff57f3587d83f8cb977fb594783c35047909a58
Camille Crapat <camille.capt@gmail.com>
2023-05-07 18:33:27 +0200

    responsive error

--- src/pages/FirstConnexion/index.jsx
+++ src/pages/FirstConnexion/index.jsx
@@ -8,24 +8,26 @@ function FirstConnexion() {
     <>
       <Header />
       <section className="connexion">
-        <div className="connexion--forms">
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Nouveau mot de passe"
-            className="input"
-          />
-          <input
-            type="password"
-            name="passwordReapeat"
-            id="passwordReapeat"
-            placeholder="Répétez le mot de passe"
-            className="input"
-          />
+        <div className="connexionContainer">
+          <div className="connexion--forms">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Nouveau mot de passe"
+              className="input"
+            />
+            <input
+              type="password"
+              name="passwordReapeat"
+              id="passwordReapeat"
+              placeholder="Répétez le mot de passe"
+              className="input"
+            />
+          </div>
+          <button>Changer le mot de passe</button>
+          <button className="buttonDeconnexion">Déconnexion</button>
         </div>
-        <button>Changer le mot de passe</button>
-        <button className="buttonDeconnexion">Déconnexion</button>
       </section>
       <Footer />
     </>

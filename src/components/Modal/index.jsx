9ebcbea62d0a0f29762277d227e3c37c1ff58bb6
Camille Crapat <camille.capt@gmail.com>
2023-05-18 14:43:11 +0200

    modification modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,14 +2,22 @@ import "./index.css";
 import Close from "../../assets/img/circle-xmark-solid.svg";
 
 function Modal({ modalOpen, setModalOpen, title, children }) {
+  if (modalOpen) {
+    document.getElementsByTagName("html")[0].style.overflowY = "hidden";
+  } else {
+  }
+  const closeModal = () => {
+    setModalOpen(!modalOpen);
+    document.getElementsByTagName("html")[0].style.overflowY = "auto";
+  };
   return (
     <>
-      <div className="bgrdModal" onClick={() => setModalOpen(!modalOpen)}></div>
+      <div className="bgrdModal" onClick={() => closeModal()}></div>
       <div className="center">
         <div className="modal">
           <div className="modalHeader">
             <div className="title">{title}</div>
-            <div className="close" onClick={() => setModalOpen(!modalOpen)}>
+            <div className="close" onClick={() => closeModal()}>
               <img src={Close} alt="icône de clôture de modal" />
             </div>
           </div>

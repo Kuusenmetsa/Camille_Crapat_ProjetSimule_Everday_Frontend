7df2a58ef064f3cd4a14f460ecdd9cf12eba836e
Camille Crapat <camille.capt@gmail.com>
2023-06-09 17:48:14 +0200

    changement unité d'affichage

--- src/pages/AllPosts/index.jsx
+++ src/pages/AllPosts/index.jsx
@@ -41,9 +41,9 @@ function AllPosts() {
             </div> */}
           <Post id="1" />
           <Post id="2" />
-          <Post id="3" />
+          {/*<Post id="3" />
           <Post id="4" />
-          <Post id="5" />
+          <Post id="5" />*/}
         </div>
       </div>
       <Footer />
